9fe1474c93759c78e130aec79f9d0a83991124fa
qinghua <qinghua.liu@thoughtworks.com>
2020-07-24 16:39:17 +0800

    create sp activity

--- app/src/main/java/com/thoughtworks/myapplication/MainActivity.java
+++ app/src/main/java/com/thoughtworks/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.thoughtworks.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.go_to_sp_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createSPActivity();
+            }
+        });
+    }
+
+    private void createSPActivity() {
+        Intent intent = new Intent(this, SPActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/thoughtworks/myapplication/SPActivity.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_p_activty);
+    }
+}
